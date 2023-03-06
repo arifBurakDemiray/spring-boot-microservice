add08bdc3086a6e548ae54431caa02bacdabe9c3
Arif Burak Demiray <burakdemiray09@hotmail.com>
2023-03-06 11:13:20 +0300

    feat: initial commit with eureka impl

---
+++ image/src/main/java/com/demiray/image/Image.java
@@ -0,0 +1,15 @@
+package com.demiray.image;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Image {
+    private int id;
+    private String name;
+    private String url;
+
+}
---
+++ zuul/src/main/java/com/demiray/zuul/JwtConfig.java
@@ -0,0 +1,26 @@
+package com.demiray.zuul;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class JwtConfig {
+    @Value("${security.jwt.uri:/auth/**}")
+    private String Uri;
+
+    @Value("${security.jwt.header:Authorization}")
+    private String header;
+
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:#{24*60*60}}")
+    private int expiration;
+
+    @Value("${security.jwt.secret:JwtSecretKey}")
+    private String secret;
+
+    // getters ...
+}
